63c77e76bf7f32770987fa36d864f1011b3513d8
alisonnbarbosaa <alisondevbarbosa@gmail.com>
2024-12-15 18:49:38 -0300

    add hover

--- src/components/CalculatorButtons.jsx
+++ src/components/CalculatorButtons.jsx
@@ -10,27 +10,27 @@ export default function CalculatorButtons({
   return (
     <section className="h-3/5 text-2xl flex flex-wrap justify-between items-center">
       <button
-        className="w-16 h-16 rounded-full uppercase text-stone-800 bg-gradient-to-tl from-stone-500 to-stone-300"
+        className="w-16 h-16 rounded-full uppercase text-stone-800 bg-gradient-to-tl from-stone-500 to-stone-300 hover:bg-gradient-to-tl hover:from-stone-400 hover:to-stone-200 duration-150"
         onClick={onClear}
       >
         ac
       </button>
 
       <button
-        className="w-16 h-16 rounded-full uppercase text-stone-800 bg-gradient-to-tl from-stone-500 to-stone-300"
+        className="w-16 h-16 rounded-full uppercase text-stone-800 bg-gradient-to-tl from-stone-500 to-stone-300 hover:bg-gradient-to-tl hover:from-stone-400 hover:to-stone-200 duration-150"
         onClick={onToggleSign}
       >
         &plusmn;
       </button>
 
       <button
-        className="w-16 h-16 rounded-full uppercase text-stone-800 bg-gradient-to-tl from-stone-500 to-stone-300"
+        className="w-16 h-16 rounded-full uppercase text-stone-800 bg-gradient-to-tl from-stone-500 to-stone-300 hover:bg-gradient-to-tl hover:from-stone-400 hover:to-stone-200 duration-150"
         onClick={() => onButtonClick("%")}
       >
         %
       </button>
       <button
-        className="w-16 h-16 rounded-full text-3xl text-stone-50 bg-gradient-to-tl from-amber-700 to-amber-400"
+        className="w-16 h-16 rounded-full text-3xl text-stone-50 bg-gradient-to-tl from-amber-700 to-amber-400 hover:bg-gradient-to-tl hover:from-amber-600 hover:to-amber-300 duration-150"
         onClick={() => onButtonClick("/")}
       >
         &divide;
@@ -39,7 +39,7 @@ export default function CalculatorButtons({
       {[7, 8, 9].map((num) => (
         <button
           key={num}
-          className="w-16 h-16 rounded-full text-stone-50 bg-gradient-to-tl from-stone-900 to-stone-600"
+          className="w-16 h-16 rounded-full text-stone-50 bg-gradient-to-tl from-stone-900 to-stone-600 hover:bg-gradient-to-tl hover:from-stone-800 hover:to-stone-500 duration-150"
           onClick={() => onButtonClick(num)}
         >
           {num}
@@ -47,7 +47,7 @@ export default function CalculatorButtons({
       ))}
 
       <button
-        className="w-16 h-16 rounded-full text-3xl text-stone-50 bg-gradient-to-tl from-amber-700 to-amber-400"
+        className="w-16 h-16 rounded-full text-3xl text-stone-50 bg-gradient-to-tl from-amber-700 to-amber-400 hover:bg-gradient-to-tl hover:from-amber-600 hover:to-amber-300 duration-150"
         onClick={() => onButtonClick("*")}
       >
         x
@@ -56,7 +56,7 @@ export default function CalculatorButtons({
       {[4, 5, 6].map((num) => (
         <button
           key={num}
-          className="w-16 h-16 rounded-full uppercase text-stone-50 bg-gradient-to-tl from-stone-900 to-stone-600"
+          className="w-16 h-16 rounded-full text-stone-50 bg-gradient-to-tl from-stone-900 to-stone-600 hover:bg-gradient-to-tl hover:from-stone-800 hover:to-stone-500 duration-150"
           onClick={() => onButtonClick(num)}
         >
           {num}
@@ -64,7 +64,7 @@ export default function CalculatorButtons({
       ))}
 
       <button
-        className="w-16 h-16 rounded-full text-3xl text-stone-50 bg-gradient-to-tl from-amber-700 to-amber-400"
+        className="w-16 h-16 rounded-full text-3xl text-stone-50 bg-gradient-to-tl from-amber-700 to-amber-400 hover:bg-gradient-to-tl hover:from-amber-600 hover:to-amber-300 duration-150"
         onClick={() => onButtonClick("-")}
       >
         -
@@ -73,7 +73,7 @@ export default function CalculatorButtons({
       {[1, 2, 3].map((num) => (
         <button
           key={num}
-          className="w-16 h-16 rounded-full uppercase text-stone-50 bg-gradient-to-tl from-stone-900 to-stone-600"
+          className="w-16 h-16 rounded-full text-stone-50 bg-gradient-to-tl from-stone-900 to-stone-600 hover:bg-gradient-to-tl hover:from-stone-800 hover:to-stone-500 duration-150"
           onClick={() => onButtonClick(num)}
         >
           {num}
@@ -81,31 +81,31 @@ export default function CalculatorButtons({
       ))}
 
       <button
-        className="w-16 h-16 rounded-full text-3xl text-stone-50 bg-gradient-to-tl from-amber-700 to-amber-400"
+        className="w-16 h-16 rounded-full text-3xl text-stone-50 bg-gradient-to-tl from-amber-700 to-amber-400 hover:bg-gradient-to-tl hover:from-amber-600 hover:to-amber-300 duration-150"
         onClick={() => onButtonClick("+")}
       >
         +
       </button>
       <button
-        className="w-16 h-16 rounded-full bg-gradient-to-tl from-stone-900 to-stone-600 uppercase text-stone-50"
+        className="w-16 h-16 rounded-full bg-gradient-to-tl from-stone-900 to-stone-600 uppercase text-stone-50 hover:bg-gradient-to-tl hover:from-stone-800 hover:to-stone-500 duration-150"
         onClick={() => onButtonClick(0)}
       >
         0
       </button>
       <button
-        className="w-16 h-16 rounded-full bg-gradient-to-tl from-stone-900 to-stone-600 uppercase text-stone-50"
+        className="w-16 h-16 rounded-full bg-gradient-to-tl from-stone-900 to-stone-600 uppercase text-stone-50 hover:bg-gradient-to-tl hover:from-stone-800 hover:to-stone-500 duration-150"
         onClick={() => onButtonClick(".")}
       >
         .
       </button>
       <button
-        className="w-16 h-16 rounded-full bg-gradient-to-tl from-stone-900 to-stone-600 uppercase text-stone-50 flex items-center justify-center"
+        className="w-16 h-16 rounded-full bg-gradient-to-tl from-stone-900 to-stone-600 uppercase text-stone-50 flex items-center justify-center hover:bg-gradient-to-tl hover:from-stone-800 hover:to-stone-500 duration-150"
         onClick={() => onDelete()}
       >
         <img src={btdelete} alt="delete" className="w-8" />
       </button>
       <button
-        className="w-16 h-16 rounded-full text-3xl text-stone-50 bg-gradient-to-tl from-amber-700 to-amber-400"
+        className="w-16 h-16 rounded-full text-3xl text-stone-50 bg-gradient-to-tl from-amber-700 to-amber-400 hover:bg-gradient-to-tl hover:from-amber-600 hover:to-amber-300 duration-150"
         onClick={() => onEqual()}
       >
         =
